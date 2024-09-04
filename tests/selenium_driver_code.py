60c2e9c8a394f6ea2721bf454ce5b1ac52e3471e
Daniel Alonso <daniel.a@trundl.com>
2024-09-04 08:53:18 -0700

    chrome driver test 7

--- tests/selenium_driver_code.py
+++ tests/selenium_driver_code.py
@@ -17,11 +17,12 @@ app_url = 'http://127.0.0.1:5000/'
 print("Navigating to:", app_url)
 #driver.get(app_url)
 #service = Service(executable_path=r'/usr/local/bin/chromedriver')
-#options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 #options.add_argument('--headless')
-#options.add_argument('--no-sandbox')
+options.add_argument('--no-sandbox')
 #options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome()
+options.add_argument('--remote-debugging-pipe')
+driver = webdriver.Chrome(options=options)
 driver.get(app_url)
 
 
